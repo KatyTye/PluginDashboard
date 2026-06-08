07172ffb57c0dc431942bf89c9bef23d6712f57c
KatyTye <d.boy.dragoz@gmail.com>
2026-06-08 17:30:40 +0200

    feat: Added footer and updated header styles

---
+++ src/components/Footer.tsx
@@ -0,0 +1,33 @@
+import { PiChatsBold } from "react-icons/pi";
+import { NavLink } from "react-router";
+
+export default function Footer() {
+
+	return (<footer className="flex flex-col gap-10 md:grid md:grid-cols-2 items-center bg-(--background-second-color) p-4
+		md:pl-20 md:pr-20">
+		<div>
+			<p className="text-center md:text-left">SEssentials</p>
+			<p className="text-(--text-second-color) text-[14px] text-center md:text-left">
+				© 2026 SEssentials Plugin. All rights reserved.
+			</p>
+		</div>
+		<nav className="flex flex-wrap gap-10 md:ml-auto justify-center">
+			<NavLink to={"/contact"} className="border-b-2 border-transparent transition-all [&.active]:border-(--special-color) 
+			hover:border-(--special-color) duration-500 text-[14px] [&.active]:text-(--text-color) text-(--text-second-color)">
+				Contact Us
+			</NavLink>
+
+			<NavLink to={"/terms"} className="border-b-2 border-transparent transition-all [&.active]:border-(--special-color) 
+			hover:border-(--special-color) duration-500 text-[14px] [&.active]:text-(--text-color) text-(--text-second-color)">
+				Copyright and Terms
+			</NavLink>
+
+			<a href="https://discord.gg/xxxxxxxx" target="_blank" className="flex gap-2 text-[14px] items-center">
+				<PiChatsBold className="w-3.5 h-3.5" />
+				<span>
+					Discord
+				</span>
+			</a>
+		</nav>
+	</footer>)
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,36 +4,35 @@ import { Link, NavLink } from "react-router";
 
 export default function Header() {
 
-	return (<header className="top-content grid grid-cols-3 items-center bg-(--background-second-color) p-4">
+	return (<header className="top-content grid gap-5 md:gap-0 md:grid-cols-3 items-center bg-(--background-second-color) p-4
+		md:pl-20 md:pr-20 justify-center grid-cols-1">
 
-		<Link to={"/"} className="text-2xl font-bold w-fit hover:text-(--special-color) duration-500">
+		<Link to={"/"} className="text-2xl text-center font-bold md:w-fit hover:text-(--special-color) duration-500">
 			SEssentials
 		</Link>
 
-		<nav className="flex gap-10 justify-center">
-			<NavLink to={"/"} className="border-b-2 border-transparent transition-all [&.active]:border-(--special-color) 
-			hover:border-(--special-color) duration-500">
-				Home
-			</NavLink>
+		<nav className="grid gap-5 md:gap-10 justify-center md:grid-cols-3 md:flex">
 			<NavLink to={"/features"} className="border-b-2 border-transparent transition-all [&.active]:border-(--special-color) 
-			hover:border-(--special-color) duration-500">
+			hover:border-(--special-color) duration-500 [&.active]:text-(--text-color) text-(--text-second-color) text-center">
 				Features
 			</NavLink>
 			<NavLink to={"/docs"} className="border-b-2 border-transparent transition-all [&.active]:border-(--special-color) 
-			hover:border-(--special-color) duration-500">
-				Docs
+			hover:border-(--special-color) duration-500 [&.active]:text-(--text-color) text-(--text-second-color) text-center">
+				Documentation
 			</NavLink>
 			<NavLink to={"/support"} className="border-b-2 border-transparent transition-all [&.active]:border-(--special-color) 
-			hover:border-(--special-color) duration-500">
+			hover:border-(--special-color) duration-500 [&.active]:text-(--text-color) text-(--text-second-color) text-center">
 				Support
 			</NavLink>
 		</nav>
 
-		<div className="flex gap-10 ml-auto">
-			<NavLink to={"/profile"} className="w-6 [&.active]:text-(--special-color) hover:text-(--special-color) duration-500">
+		<div className="md:flex gap-10 justify-center ml-auto hidden">
+			<NavLink to={"/profile"} className="w-6 [&.active]:text-(--text-color) hover:text-(--special-color)
+			duration-500 text-(--text-second-color)">
 				<FaRegUserCircle />
 			</NavLink>
-			<button className="cursor-pointer w-6 [&.active]:text-(--special-color) hover:text-(--special-color) duration-500">
+			<button className="cursor-pointer w-6 [&.active]:text-(--text-color) hover:text-(--special-color)
+			duration-500 text-(--text-second-color)">
 				<MdOutlineSettings />
 			</button>
 		</div>
