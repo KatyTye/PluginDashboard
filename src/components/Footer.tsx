e5665165f61a53cb1cdcf3b6a5e73c7a33ca53fa
KatyTye <d.boy.dragoz@gmail.com>
2026-06-11 08:15:13 +0200

    refactor: Added link rel's for better seo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,16 +13,17 @@ export default function Footer() {
 		</div>
 		<nav className="flex flex-wrap gap-10 md:ml-auto justify-center">
 			<NavLink to={"/support"} className="border-b-2 border-transparent transition-all [&.active]:border-(--special-color) 
-			hover:border-(--special-color) duration-500 text-[14px] [&.active]:text-(--text-color) text-(--text-second-color)">
-				Contact Us
+			hover:border-(--special-color) duration-500 text-[14px] [&.active]:text-(--text-color) text-(--text-second-color)"
+				rel="alternate">Contact Us
 			</NavLink>
 
 			<NavLink to={"/terms"} className="border-b-2 border-transparent transition-all [&.active]:border-(--special-color) 
-			hover:border-(--special-color) duration-500 text-[14px] [&.active]:text-(--text-color) text-(--text-second-color)">
-				Copyright and Terms
+			hover:border-(--special-color) duration-500 text-[14px] [&.active]:text-(--text-color) text-(--text-second-color)"
+				rel="alternate">Copyright and Terms
 			</NavLink>
 
-			<a href="https://discord.gg/xxxxxxxx" target="_blank" className="flex gap-2 text-[14px] items-center">
+			<a href="https://discord.gg/xxxxxxxx" target="_blank" className="flex gap-2 text-[14px] items-center"
+				rel="noopener noreferrer">
 				<PiChatsBold className="w-3.5 h-3.5" />
 				<span>
 					Discord
