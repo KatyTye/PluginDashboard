33e709d969ee56095831e5f7caf331eff4db4e24
KatyTye <d.boy.dragoz@gmail.com>
2026-06-20 18:55:45 +0200

    fix: Changed how it finds the latest version

--- src/helpers/findLatests.ts
+++ src/helpers/findLatests.ts
@@ -1,11 +1,6 @@
 export function findLatestValue<T extends Record<string, any>, K extends keyof T, W extends keyof T>
 	(array: T[], key: K, where: W) {
-
-	const gottenKeys = array.map(obj => {
-		if (obj[where] === true) {
-			return obj[key]
-		}
-	})
-
-	return gottenKeys[1]
+	
+	const filtered = array.filter(obj => obj[where] === true).reverse()
+	return filtered[filtered.length - 1]?.[key]
 }
